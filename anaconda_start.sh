64c7d1f4b54ce214e28b3eccb7eb296d85cfeaf8
alistairwalsh <alistair.walsh@me.com>
2015-05-17 13:17:02 +0000

    modified repositories file

--- anaconda_start.sh
+++ anaconda_start.sh
@@ -2,8 +2,13 @@
 
 wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.2.0-Linux-x86_64.sh
 
-bash -b Anaconda-2.2.0-Linux-x86_64.sh
+bash Anaconda-2.2.0-Linux-x86_64.sh -b
 
-export PATH=~/anaconda/bin:$PATH
+echo "export PATH=~/anaconda/bin:$PATH" >> .bashrc 
+conda update conda
+sudo apt-get update && sudo apt-get install git-core -y
 
-conda install --channel https://conda.binstar.org/jorge qiime
+conda config --add channels http://conda.binstar.org/jorge
+
+sudo apt-get --force-yes -y install python-dev libncurses5-dev libssl-dev libzmq-dev libgsl0-dev openjdk-6-jdk libxml2 libxslt1.1 libxslt1-dev ant git subversion build-essential zlib1g-dev libpng12-dev libfreetype6-dev mpich2 libreadline-dev gfortran unzip libmysqlclient18 libmysqlclient-dev ghc sqlite3 libsqlite3-dev libc6-i386 libbz2-dev tcl-dev tk-dev r-base r-base-dev libatlas-dev libatlas-base-dev liblapack-dev swig libhdf5-serial-dev
+
