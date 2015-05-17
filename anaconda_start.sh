bc3cb31555f11b7fca24821592d676a0d6e3d2c3
alistairwalsh <alistair.walsh@me.com>
2015-05-17 13:57:20 +0000

    added more lines without testing

--- anaconda_start.sh
+++ anaconda_start.sh
@@ -14,9 +14,21 @@ git config --global credential.helper cache
 
 git config --global credential.helper 'cache --timeout=3600'
 # Set the cache to timeout after 1 hour (setting is in seconds)
-git commit --amend --reset-author
-
 conda config --add channels http://conda.binstar.org/jorge
+conda install qiime -y
+
+sudo mv sources.list /etc/apt/sources.list -f
+
+gpg --keyserver pgpkeys.mit.edu --recv-key 51716619E084DAB9
+gpg -a --export 51716619E084DAB9 | sudo apt-key add -
+
+sudo apt-get -y update
+
+git clone git://github.com/qiime/qiime-deploy.git
+git clone git://github.com/qiime/qiime-deploy-conf.git
+cd qiime-deploy/
+python qiime-deploy.py $HOME/qiime_software/ -f $HOME/qiime-deploy-conf/qiime-1.9.0/qiime.conf --force-remove-failed-dirs
+source $HOME/.bashrc
 
 sudo apt-get --force-yes -y install python-dev libncurses5-dev libssl-dev libzmq-dev libgsl0-dev openjdk-6-jdk libxml2 libxslt1.1 libxslt1-dev ant git subversion build-essential zlib1g-dev libpng12-dev libfreetype6-dev mpich2 libreadline-dev gfortran unzip libmysqlclient18 libmysqlclient-dev ghc sqlite3 libsqlite3-dev libc6-i386 libbz2-dev tcl-dev tk-dev r-base r-base-dev libatlas-dev libatlas-base-dev liblapack-dev swig libhdf5-serial-dev
 
